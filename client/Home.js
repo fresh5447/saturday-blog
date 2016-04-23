8df05bac055f4be67b90eb3e9a249fba4c462876
fresh5447 <doug@kosmojo.com>
2016-04-23 15:42:13 -0600

    skills section basic layout

--- client/Home.js
+++ client/Home.js
@@ -20,6 +20,22 @@ var Home = React.createClass({
             </p>
           </div>
         </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="card">
+                <img className="card-img-top" src="http://i.onionstatic.com/onion/2508/0/original/600.jpg" alt="Card image cap"/>
+                <div className="card-block">
+                  <h4 className="card-title">Full Stack Developer</h4>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <h3> A summary of my skills </h3>
+              <p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+            </div>
+          </div>
+        </div>
       </div>
       )
   }
